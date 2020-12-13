01c0ccbc08a57940780f739c43352bc620fbaa1c
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2020-12-13 14:44:48 +0530

    Fix main method

--- src/main/java/in/praj/swfout/App.java
+++ src/main/java/in/praj/swfout/App.java
@@ -17,7 +17,7 @@ public class App {
         if (args.length == 0) {
             throw new RuntimeException("Missing input file");
         }
-        return new Options(args[0], null);
+        return new Options(args[0], args.length > 1 ? args[1] : "out.swf");
     }
 
     void prepareInputFile(Options options) {
@@ -86,10 +86,9 @@ public class App {
     public static void main(String[] args) {
         var app = new App();
         try {
-            var exe = app.parse(args);
-            app.prepareInputFile(exe);
-
-            // TODO: Fill up
+            var opts = app.parse(args);
+            app.prepareInputFile(opts);
+            app.extractSwf(opts);
         } catch (RuntimeException e) {
             System.out.println("Error: " + e.getMessage());
         } finally {
